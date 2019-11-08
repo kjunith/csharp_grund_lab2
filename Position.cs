3e3795560660934dfd79fd3d139f3f44d9301347
kjunith <jimmie.maatta@gmail.com>
2019-11-08 12:01:35 +0100

    Fri 8 Nov 2019

---
+++ Position.cs
@@ -0,0 +1,85 @@
+﻿using System;
+namespace Labb2
+{
+    public class Position
+    {
+        private double _x;
+        private double _y;
+
+        public double X
+        {
+            get => _x;
+            set
+            {
+                if (value >= 0)
+                {
+                    _x = value;
+                }
+            }
+        }
+
+        public double Y
+        {
+            get => _y;
+            set
+            {
+                if (value >= 0)
+                {
+                    _y = value;
+                }
+            }
+        }
+
+        public Position(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double Length()
+        {
+            return Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+        }
+
+        public bool Equals(Position p)
+        {
+            return X.Equals(p.X) && Y.Equals(p.Y);
+        }
+
+        public Position Clone()
+        {
+            return new Position(X, Y);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("({0}, {1})", X, Y);
+        }
+
+        public static bool operator > (Position p1, Position p2)
+        {
+            return p1.Length() > p2.Length();
+        }
+
+        public static bool operator < (Position p1, Position p2)
+        {
+            return p1.Length() < p2.Length();
+        }
+
+        public static Position operator + (Position p1, Position p2)
+        {
+            return new Position(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        public static Position operator -(Position p1, Position p2)
+        {
+            return new Position(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static double operator % (Position p1, Position p2)
+        {
+            return Math.Sqrt(Math.Pow(p1.X - p2.X, 2) + Math.Pow(p1.Y - p2.Y, 2));
+        }
+    }
+}
+    
---
+++ SortedPosList.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Labb2
+{
+    public class SortedPosList
+    {
+        private List<Position> posList;
+
+        public SortedPosList() {
+            posList = new List<Position>();
+        }
+
+        public int Count()
+        {
+            return posList.Capacity;
+        }
+
+        public void Add(Position p)
+        {
+            posList.Add(p);
+            posList.OrderBy(pos => pos.Length());
+        }
+
+        public bool Remove(Position p)
+        {
+            List<Position> temp = posList;
+            if (temp.Contains(p))
+            {
+                posList.Remove(p);
+                return true;
+            }
+
+            return false;
+        }
+
+        public SortedPosList Clone()
+        {
+            SortedPosList instance = new SortedPosList();
+
+            instance.posList = posList;
+
+            return instance;
+        }
+
+        public SortedPosList CircleContent(Position centerPos, double radius)
+        {
+            SortedPosList instance = new SortedPosList();
+
+            foreach (Position p in posList)
+            {
+                if (Math.Pow(p.X - centerPos.X, 2) + Math.Pow(p.Y - centerPos.Y, 2) < Math.Pow(radius, 2))
+                {
+                    instance.Add(p);
+                }
+            }
+
+            return instance;
+        }
+
+        public static SortedPosList operator +(SortedPosList posList1, SortedPosList posList2)
+        {
+            SortedPosList instance = posList1.Clone();
+
+            foreach (Position p in posList2.posList)
+            {
+                instance.posList.Add(p);
+            }
+
+            return instance;
+        }
+
+        public Position GetPosition(int pos)
+        {
+            return posList[pos];
+        }
+
+        public static SortedPosList operator -(SortedPosList posList1, SortedPosList posList2)
+        {
+            SortedPosList instance = posList1.Clone();
+
+            foreach (Position p in posList2.posList)
+            {
+                if (instance.posList.Contains(p))
+                {
+                    instance.Remove(p);
+                }
+            }
+
+            return instance;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("SortedPosList:\n");
+
+            for (int i = 0; i < posList.Count; i++)
+            {
+                builder.Append(posList[i].ToString());
+
+                if (i != posList.Count - 1)
+                {
+                    builder.Append(", ");
+                }
+            }
+
+            return builder.ToString();
+        }
+    }
+}
